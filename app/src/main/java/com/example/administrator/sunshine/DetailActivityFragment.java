3862753adfa9b2e26065134d8dd6f03caba66aaa
ishtiaque <ishtiaque.asad@outlook.com>
2015-12-07 20:19:35 +0600

    after all repair unit 3 just 2nd activity

--- app/src/main/java/com/example/administrator/sunshine/DetailActivityFragment.java
+++ app/src/main/java/com/example/administrator/sunshine/DetailActivityFragment.java
@@ -1,7 +1,8 @@
 package com.example.administrator.sunshine;
 
-import android.os.Bundle;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,11 +19,11 @@ public class DetailActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        String message=getActivity().getIntent().getStringExtra("hello");
-
+        Intent intent =getActivity().getIntent();
+        String a = intent.getStringExtra("hello");
         View view=inflater.inflate(R.layout.fragment_detail, container, false);
-        TextView showResult = (TextView)view.findViewById(R.id.detailActivity_textview);
-        showResult.setText(message);
+        TextView detailTextview = (TextView) view.findViewById(R.id.detail_textview);
+        detailTextview.setText(a);
 
         return view;
     }
